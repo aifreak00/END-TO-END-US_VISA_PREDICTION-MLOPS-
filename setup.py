d9ab2ffe66dd3111eff06300b476bddfb58f6cb1
aifreak00 <prathameshettam2001@gmail.com>
2024-06-11 20:05:33 +0530

    start

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="aifreak00",
+    author_email="prathameshettam2001@gmail.com",
+    packages=find_packages()
+)
